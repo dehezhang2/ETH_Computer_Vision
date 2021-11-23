aaee1881c63d1314881f6f5eb83888472d90fc61
dehezhang2 <dehezhang2@gmail.com>
2021-11-23 15:10:57 +0100

    assignment4 except training testing

--- assignment4/Computer_Vision_Assignment4.md
+++ assignment4/Computer_Vision_Assignment4.md
@@ -8,6 +8,17 @@
 >>> python line_fitting.py
 (500,)
 Estimated coefficients (true, linear regression, RANSAC):
-1 10 0.6159656578755457 8.96172714144364 1.0103652266590768 9.807457331484198
+(ground truth) 									1 10 
+(estimation from least-squares) 0.6159656578755457 8.96172714144364 (estimation from RANSAC)			  1.0103652266590768 9.807457331484198
+
 ```
 
+## 3 Multi-View Stereo
+
+### 3.2.2 Differentiable Warping
+
+Equation of correspondence: if we assume the word coordinate is $\bf X$
+$$
+d_j{\bf p}= {\bf K}_0({\bf R}_0{\bf X}+{\bf t}_0) \mapsto d_j{\bf K}_0^{-1}{\bf p}= ({\bf R}_0{\bf X}+{\bf t}_0)\\
+{\bf p}_{ij}= {\bf K}_i({\bf R}_{0,i}({\bf R}_0{\bf X}+{\bf t}_0) + {\bf t}_{0,i}) = {\bf K}_i(d_j{\bf R}_{0,i}{\bf K}_0^{-1}{\bf p} + {\bf t}_{0,i})\\
+$$
--- assignment4/codes/mvs/models/net.py
+++ assignment4/codes/mvs/models/net.py
@@ -46,11 +46,10 @@ class Net(nn.Module):
         with torch.no_grad():
             # photometric confidence
             prob_volume_sum4 = 4 * F.avg_pool3d(F.pad(prob_volume.unsqueeze(1), 
-                                            pad=(0, 0, 0, 0, 1, 2)), (4, 1, 1), stride=1, padding=0).squeeze(1)
+                                            pad=(0, 0, 0, 0, 1, 2)), (4, 1, 1), stride=1, padding=0).squeeze(1)                                  
             depth_index = depth_regression(prob_volume, depth_values=torch.arange(D, 
-                                            device=prob_volume.device, dtype=torch.float)).long()
+                                              device=prob_volume.device,dtype=torch.float).unsqueeze(0).repeat(B,1)).long()
             photometric_confidence = torch.gather(prob_volume_sum4, 1, depth_index.unsqueeze(1)).squeeze(1)
-
         return {
             "depth": depth, 
             "photometric_confidence": photometric_confidence
